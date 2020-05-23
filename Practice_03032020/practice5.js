ea20d2bcbdb14e8b882c68d568554b3159f3b6e9
Greg Martin <gregm31@live.com>
2020-05-23 11:55:01 +1000

    Using this keyword in nested functions and callbacks

--- Practice_03032020/practice5.js
+++ Practice_03032020/practice5.js
@@ -83,6 +83,19 @@
 // const printBio = person.printBio;
 
 const annoyer = {
-	phrases: [ 'literally', 'cray cray', "I can't event", 'Totes!', 'YOLO', "Can't stop, won't stop" ]
-	// start
+	phrases: [ 'literally', 'cray cray', "I can't event", 'Totes!', 'YOLO', "Can't stop, won't stop" ],
+	pickPhrase() {
+		const { phrases } = this;
+		const idx = Math.floor(Math.random() * phrases.length);
+		return phrases[idx];
+	},
+	start() {
+		this.timerId = setInterval(() => {
+			console.log(this.pickPhrase());
+		}, 3000);
+	},
+	stop() {
+		clearInterval(this.timerId);
+		console.log('Deliciously stopped');
+	}
 };
